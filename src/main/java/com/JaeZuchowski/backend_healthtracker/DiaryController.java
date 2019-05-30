18a00ec0a33f144586b6b7bbb674ac04aac9cc7b
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-29 23:52:24 -0700

    added routes

--- src/main/java/com/JaeZuchowski/backend_healthtracker/DiaryController.java
+++ src/main/java/com/JaeZuchowski/backend_healthtracker/DiaryController.java
@@ -2,14 +2,30 @@ package com.JaeZuchowski.backend_healthtracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/diary")
 public class DiaryController {
 
     @Autowired
     DiaryRepo diaryRepo;
 
-    @PostMapping("/DiaryActivity")
-    public
+    @PostMapping("/post")
+    public Diary createDiary(
+            @RequestBody Diary diary
+    ) {
+        diary = diaryRepo.save(diary);
+        return diary;
+    }
+
+    @GetMapping("/get")
+    public List getDiaryResults() {
+        return diaryRepo.findAll();
+    }
 }
