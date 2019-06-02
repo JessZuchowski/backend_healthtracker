3ae9f24fb3744195a0ae3c328ead6cfdfe7ffa3f
jaezuchowski <jess.zuchowski@gmail.com>
2019-06-02 15:12:18 -0700

    removed security, added rest

--- src/main/java/com/JaeZuchowski/backend_healthtracker/DiaryController.java
+++ src/main/java/com/JaeZuchowski/backend_healthtracker/DiaryController.java
@@ -2,14 +2,11 @@ package com.JaeZuchowski.backend_healthtracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/diary")
 public class DiaryController {
 
